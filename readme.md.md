ea25355a0b601376aad0d8fec24090b00043eaba
JavierGrijalba <jgrijalaz@gmail.com>
2023-01-17 10:43:16 +0100

    main en beta

---
+++ readme.md.md
@@ -0,0 +1,12 @@
+# Proyecto Java OOP
++ Crear una clase: Producto
+	* atributos
+	* constructores:
+		* vacio
+		* con todos los atributos
+	* metodos getter/setter
+	* toStrint
++ Crear clase main
+	+ Crear 5 productos
+	+ Crear un array de productos
+	+ 
--- src/com/productos/Main.java
+++ src/com/productos/Main.java
@@ -1,4 +1,26 @@
 package com.productos;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Product product1 = new Product(1, "escoba", "barre el suelo", 6.8);
+        Product product2 = new Product(2, "valleta", "de mano", 6.8);
+        Product product3 = new Product(3, "agua", "se bebe", 0.0);
+        Product product4 = new Product(4, "chupa-chups", "esta rico", 0.25);
+        Product product5 = new Product(5, "ventana", "transparente", 200.0);
+
+        Product[] arrayDeProductos = new Product[]{product1, product2, product3, product4, product5};
+
+        System.out.printf("%10s %10s %10s %10s\n***********************************",
+                "NumProducto", "Nombre", "Descripcion", "Precio");
+
+        for (int i = 0; i < arrayDeProductos.length; i++) {
+            arrayDeProductos[i].toString();
+            System.out.println();
+        }
+    }
+
 }
+
+
